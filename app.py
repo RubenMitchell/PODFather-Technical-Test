2e3b90289d1600d81c1a8baf907be73b27b545fc
RubenMitchell <148381967+RubenMitchell@users.noreply.github.com>
2024-04-28 12:57:59 +0100

    Added basic filters
    Can add concurrent filters from any category from dynamically generated filter options.

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import pandas as pd
 
+# Flask API admin
 app = Flask(__name__)
+app.secret_key = 'secret_key'
 
-#load in the dataset
+#Datatset load in
 try:
     global_data = pd.read_excel('pod-data.xlsx', engine='openpyxl')
 except Exception as e:
     print("Failed to load data:", e)
     global_data = pd.DataFrame()  # Use an empty DataFrame as a fallback
 
+#helper function to return dataset subject to filters as html table
+def get_table():
+    data = global_data
+    for column, values in session['filter_dict'].items():
+        if values:
+            #Numerical column values are returned as strings from the form - this converts them to back to integers
+            if data[column].dtype in ('int64', 'int32'):
+                values = [int(value) for value in values]
+            data = data[data[column].isin(values)] #returns all entries with the value in column being one of the filter values
+    return data.to_html(index=False)
+
 @app.route('/')
 def load():
-    return render_template('layout.html', table_data = global_data.to_html(index=False))
+    session['filter_dict'] = {column: [] for column in global_data.columns}# Reset the filters on reload
+    unique_entries = {column: global_data[column].unique() for column in global_data.columns[:-2]}
+    return render_template('layout.html', table_data = get_table(), unique_entries = unique_entries)    
+
+@app.route('/add_filter', methods=['POST'])
+def add_filter():
+    column = request.form.get('column')
+    value = request.form.get('value')
+    session['filter_dict'][column].append(value)
+    session.modified = True
+    return get_table()
 
 
 if __name__ == '__main__':
---
+++ static/script.js
@@ -0,0 +1,24 @@
+function postRequest(url, params) {
+    fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(params).toString()
+    })
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('table').innerHTML = html;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+document.querySelectorAll('.filter').forEach(filter => {
+    const options = filter.querySelectorAll('.option');
+    // Setup option listeners within each dropdown
+    options.forEach(option => {
+        option.addEventListener('click', (event) => {
+            event.stopPropagation(); // Stop click propagation
+            postRequest('/add_filter', {column: filter.getAttribute('data-column'), value: option.textContent });
+        });
+    });
+});
+
