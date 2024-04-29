3a74c18ed8cc79ba6da24667e27290ba394b6371
RubenMitchell <148381967+RubenMitchell@users.noreply.github.com>
2024-04-29 09:03:08 +0100

    Added Distribution Range Visual
    an finished CSS

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, send_file, jsonify
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import io
+import zipfile
 
 # Flask API admin
 app = Flask(__name__)
@@ -12,22 +17,30 @@ except Exception as e:
     print("Failed to load data:", e)
     global_data = pd.DataFrame()  # Use an empty DataFrame as a fallback
 
-#helper function to return dataset subject to filters as html table
-def get_table():
-    data = global_data
-    for column, values in session['filter_dict'].items():
+
+def apply_normal_filters():
+    data = global_data.copy()
+    for column, values in list(session['filter_dict'].items()):
         if values:
             #Numerical column values are returned as strings from the form - this converts them to back to integers
             if data[column].dtype in ('int64', 'int32'):
                 values = [int(value) for value in values]
             data = data[data[column].isin(values)] #returns all entries with the value in column being one of the filter values
-    return data.to_html(index=False)
+    return data
+
+def apply_range_filters(data):
+    for column, values in list(session['range_dict'].items()):
+        if values:
+            data = data[(data[column] >= values[0]) & (data[column] <= values[1])]
+    return data
 
 @app.route('/')
 def load():
-    session['filter_dict'] = {column: [] for column in global_data.columns}# Reset the filters on reload
+    session['filter_dict'] = {column: [] for column in global_data.columns[:-2]}# Reset the filters on reload
+    session['range_dict'] = {column: [] for column in global_data.columns[-2:]}
+    session['limits'] = {column: (int(global_data[column].min()), int(global_data[column].max())) for column in global_data.columns[-2:]}
     unique_entries = {column: global_data[column].unique() for column in global_data.columns[:-2]}
-    return render_template('layout.html', table_data = get_table(), unique_entries = unique_entries)    
+    return render_template('layout.html', table_data = apply_normal_filters().to_html(index = False), unique_entries = unique_entries,  limits=session['limits'])    
 
 @app.route('/add_filter', methods=['POST'])
 def add_filter():
@@ -35,7 +48,7 @@ def add_filter():
     value = request.form.get('value')
     session['filter_dict'][column].append(value)
     session.modified = True
-    return get_table()
+    return apply_normal_filters().to_html(index = False)
 
 @app.route('/remove_filter', methods=['POST'])
 def remove_filter():
@@ -43,14 +56,55 @@ def remove_filter():
     value = request.form.get('value')
     session['filter_dict'][column].remove(value)
     session.modified = True
-    return get_table()
+    return apply_normal_filters().to_html(index = False)
 
 @app.route('/clear_filter', methods=['POST'])
 def clear_filter():
     column = request.form.get('column')
     session['filter_dict'][column] = []
     session.modified = True
-    return get_table()
+    return apply_normal_filters().to_html(index = False)
+
+@app.route('/update_dists', methods=['POST'])
+def update_dists():
+    columns = [apply_normal_filters()["Estimated quantity (kg)"], apply_normal_filters()["Actual quantity (kg)"]]
+    memory_file = io.BytesIO() 
+    with zipfile.ZipFile(memory_file, 'w') as zipf:
+        for column in columns:
+            fig, ax = plt.subplots()
+            ax.hist(column, bins=column.count(), color='black', alpha=0.7)
+            session['limits'][column.name] = (int(column.min()), int(column.max()))
+            ax.set_xlim(column.min(), column.max())
+            #ax.set_xticklabels([])  # Remove x-axis tick labels
+            ax.set_yticklabels([])  # Remove y-axis tick labels
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+            ax.spines['left'].set_visible(False)  # Consider hiding or adjusting the left spine
+            ax.tick_params(axis='both', which='both', length=0)
+            pngImage = io.BytesIO()
+            plt.savefig(pngImage, format='png', bbox_inches='tight', pad_inches=0, transparent=True)
+            pngImage.seek(0)
+            plt.close(fig)
+            zipf.writestr(f'{column.name}.png', pngImage.read())
+    memory_file.seek(0)
+    session.modified = True
+    return send_file(memory_file, mimetype='application/zip')
+
+@app.route('/get_limits')
+def get_limits():
+    return jsonify(session['limits'])
+
+
+@app.route('/filter_range', methods=['POST'])
+def filter_range():
+    column = request.form.get('column')
+    lower = int(request.form.get('min'))
+    upper = int(request.form.get('max'))
+    session['range_dict'][column] = (lower, upper)
+    session.modified = True
+    return apply_range_filters(apply_normal_filters()).to_html(index = False)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -13,7 +13,7 @@ function setupTableSorting() {
                 header.className = '';
             });
             rows.sort((rowA, rowB) => {
-                const cellA = rowA.children[index].textContent.toLowerCase();  // case-insensitive comparison
+                const cellA = rowA.children[index].textContent.toLowerCase();  // case-insensitive comps
                 const cellB = rowB.children[index].textContent.toLowerCase();
                 
                 // Compare cells as numbers if possible, else as strings
@@ -27,62 +27,132 @@ function setupTableSorting() {
             });
             // Rearrange sorted rows in the table body
             rows.forEach(row => tableBody.appendChild(row));
-            // Toggle sort direction for the next click
+            
+            //Add class to header to show sort direction
             if (sortDirections[index] == 1) {
                 header.classList.add('asc');
             }
-            else
-            {
+            else{
                 header.classList.add('desc');
             }
+
+            // Toggle sort direction for the next click
             sortDirections[index] *= -1;
 
         });
     });
 }
 
-//Add tabe sorting listners when the page is loaded
-document.addEventListener("DOMContentLoaded", function() {
-    setupTableSorting()
-    document.querySelectorAll('.filter').forEach(filter => {
-        let options = filter.querySelector('.options');
-        options.style.maxHeight = 'none'; // Temporarily remove max-height restriction
-        let height = options.scrollHeight; // measure the scrollHeight
-        options.style.setProperty('--max-height', height + 'px'); // Set the custom property
-        options.style.maxHeight = ''; // Reapply the original max-height
-    });
-});
-
-function postRequest(url, params) {
-    fetch(url, {
+//Fetch Filtered table function
+function tableRequest(url, params) {
+    // Return the fetch promise to make this awaitable
+    return fetch(url, {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: new URLSearchParams(params).toString()
     })
-    .then(response => response.text())
-    .then(html => {document.getElementById('table').innerHTML = html;
-    setupTableSorting() // Re-add sorting listeners after table is updated
+    .then(response => {return response.text();})
+    .then(html => {
+        document.getElementById('table').innerHTML = html;
+        setupTableSorting();  //Add listners to new table header
     })
-    .catch(error => console.error('Error:', error));
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
-//Filtration
+//This runs intially to caluclate the height of all dropdown boxes to allow for smooth transition
+function set_dropdown_heights(){
+    document.querySelectorAll('.filter').forEach(filter => {
+        const filters = document.querySelectorAll('.filter');
+        filters.forEach(filter => {
+            const images = filter.querySelectorAll('img');
+            const ImagesLoaded = Array.from(images).map(img => {
+                if (img.complete && img.naturalHeight !== 0) {
+                    return Promise.resolve();  // Image already loaded
+                } 
+                else {
+                    return new Promise((resolve, reject) => {
+                        img.onload = resolve;
+                        img.onerror = reject;
+                    });
+                }
+            });
+        // Wait for all images within the filter to load
+        Promise.all(ImagesLoaded).then(() => {
+            const options = filter.querySelector('.options');
+            options.style.maxHeight = 'none'; // Remove max-height restriction
+            let height = options.scrollHeight;  // Measure the scrollHeight
+            options.style.setProperty('--max-height', height + 'px'); // Set the max-height CSS variable
+            options.style.maxHeight = ''; // Reapply the original max-height
+            console.log('Height set for filter:', height);
+        })
+    });
+    });
+        
+}
+
+//This runs to update the distributions after any (non-range) filters have been updated
+async function update_distributions() {
+    fetch('/update_dists', {
+        method: 'POST'
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        return JSZip.loadAsync(blob);
+    })
+    .then(zip => {
+        const imagePromises = [];
+        zip.forEach((relativePath, zipEntry) => {
+            if (zipEntry.name.endsWith('.png')) {
+                const imgPromise = zipEntry.async('blob').then(blob => {
+                    const dist = document.getElementById(zipEntry.name.replace('.png', '')); // Get the image element by ID
+                    const url = URL.createObjectURL(blob); // Create a URL object
+                    dist.src = url; // Set the image source to the URL
+                    return new Promise((resolve) => {
+                        dist.onload = () => {
+                            URL.revokeObjectURL(url); // Clean up the URL object
+                            resolve();
+                        };
+                    });
+                });
+                imagePromises.push(imgPromise);
+            }
+        });
+        return Promise.all(imagePromises); // Wait for all images to be processed
+    })
+    .then(() => {
+        fetchLimitsAndUpdate(); // This function is called only after all images have been processed
+        console.log('Limits updated');   });
+}
+
+//When the page is loaded; update the distributions, set the dropdown heights and initialise table sorting
+document.addEventListener("DOMContentLoaded", function() {
+    update_distributions();
+    set_dropdown_heights();
+    setupTableSorting();
+});
+
+//Filtration functions
 document.querySelectorAll('.filter').forEach(filter => {
     //Fetch all options for current filter
     const options = filter.querySelectorAll('.option');
     filter.querySelector('input').addEventListener('click', (event) => {
+        console.log('click');
         event.stopPropagation(); // Stop click propagation
-        filter.classList.toggle('opened');
+        filter.classList.toggle('opened'); // Toggle the opened class (to show/hide the dropdown)
     });
     
+    //This is repsonsible for updating UI if a filter is active
     function update_placeholder(){
-        count = 0;
+        count = 0; // Count the number of active filters to display
         options.forEach((i) => {
             if (i.dataset.state == 'selected') {
                 count += 1;
             }
         });
         column_name = filter.dataset.column;
+        // if active adjust placeholder accordingly and add active class 
         if (count > 0) {
             filter.querySelector('input').placeholder = column_name+'s: '+count + ' Active';
             filter.classList.add('active');
@@ -93,27 +163,166 @@ document.querySelectorAll('.filter').forEach(filter => {
         }
     }
 
-    filter.querySelector('.clear_button').addEventListener('click', (event) => {
-        event.stopPropagation(); 
-        postRequest('/clear_filter', {column: filter.getAttribute('data-column')});
-        options.forEach((i) => {i.dataset.state = 'available';}); 
-        update_placeholder();
+    //Add event listener to clear button
+    filter.querySelector('.clear_button').addEventListener('click', async (event) => {
+        event.stopPropagation(); // Prevent the event from bubbling up
+        try {
+            // Wait for the asynchronous tableRequest to complete
+            await tableRequest('/clear_filter', {column: filter.getAttribute('data-column')});
+    
+            // Now that the request has completed, proceed (this would otherwise conflict with distribution generation)
+            options.forEach((option) => {
+                option.dataset.state = 'available'; // Reset all options to available
+            });
+    
+            update_placeholder(); 
+            update_distributions(); // safely update distributions after the backend has processed
+        } catch (error) {
+            console.error('Error clearing filters:', error);
+        }
     });
 
     //Loop through each option adding a click event listener
     options.forEach(option => {
-        option.addEventListener('click', (event) => {
-            event.stopPropagation(); // Stop click propagation
-            //If option available, add filter, else remove filter
-            if (option.getAttribute('data-state') === 'available') {
-                postRequest('/add_filter', {column: filter.getAttribute('data-column'), value: option.textContent});
-                option.setAttribute('data-state', 'selected');
+        option.addEventListener('click', async (event) => {
+            event.stopPropagation(); // Prevents further propagation of the current event.
+            try {
+                const column = filter.getAttribute('data-column');
+                const value = option.textContent;
+                const state = option.getAttribute('data-state');
+                //This handles adding filters by communcating with python backend
+                if (state === 'available') {
+                    await tableRequest('/add_filter', {column, value});
+                    option.setAttribute('data-state', 'selected');
+                } else {
+                    await tableRequest('/remove_filter', {column, value});
+                    option.setAttribute('data-state', 'available');
+                }
+                update_placeholder(); // Update UI
+                update_distributions(); // Update distributions, dependent on filters 
             }
-            else {
-                postRequest('/remove_filter', {column: filter.getAttribute('data-column'), value: option.textContent});
-                option.setAttribute('data-state', 'available');
+            catch (error) {
+                console.error('Error adding/removing filter:', error);
             }
-            update_placeholder();
         });
+    });
+});
+
+
+const est_slider = document.querySelector('div[data-column="Estimated quantity (kg)"] .slider_wrapper');
+const actual_slider = document.querySelector('div[data-column="Actual quantity (kg)"] .slider_wrapper');
+
+function fetchLimitsAndUpdate() {
+    fetch('/get_limits')
+    .then(response => {return response.json(); })
+    .then(data => {
+        console.log("Received data:", data);
+        updateSliderLimits(est_slider, data['Estimated quantity (kg)'][0], data['Estimated quantity (kg)'][1]);
+        updateSliderLimits(actual_slider, data['Actual quantity (kg)'][0], data['Actual quantity (kg)'][1]);
+    })
+    .catch(error => console.error('Error fetching limits:', error));
+}
+
+function updateSliderLimits(slider, min, max) {
+    console.log('Updating slider limits:', min, max);
+    const sliderMin = slider.querySelector('.sliderMin');
+    const sliderMax = slider.querySelector('.sliderMax');
+    const inputMin = slider.querySelector('.inputMin');
+    const inputMax = slider.querySelector('.inputMax');
+    const lineMin = slider.querySelector('.lineMin');
+    const lineMax = slider.querySelector('.lineMax');
+    lineMin.style.right = `${sliderMin.offsetWidth}px`;
+    lineMax.style.left = `${sliderMin.offsetWidth - parseFloat(window.getComputedStyle(slider).fontSize)}px`;
+    sliderMin.min   = min;
+    sliderMin.max   = max;
+    sliderMin.value = min;
+    sliderMax.min   = min;
+    sliderMax.max   = max;
+    sliderMax.value = max;
+    inputMin.min    = min;
+    inputMin.max    = max;
+    inputMin.value  = min;
+    inputMax.min    = min;
+    inputMax.max    = max;
+    inputMax.value  = max;
+}
+
+
+
+document.querySelectorAll('.slider_wrapper').forEach(slider => {
+    const sliderMin = slider.querySelector('.sliderMin');
+    const sliderMax = slider.querySelector('.sliderMax');
+    const inputMin = slider.querySelector('.inputMin');
+    const inputMax = slider.querySelector('.inputMax');
+    const lineMin = slider.querySelector('.lineMin');
+    const lineMax = slider.querySelector('.lineMax');
+    const column = slider.parentElement.parentElement.getAttribute('data-column');
+    const knobWidth = parseFloat(window.getComputedStyle(slider).fontSize);
+    const offsetwdith = sliderMin.offsetWidth
+    
+    updateLinePosition();
+    
+    function updateLinePosition() {
+        max = parseFloat(sliderMax.max);
+        min = parseFloat(sliderMin.min);
+        percentage_min = (sliderMin.value - min) / (max - min);
+        percentage_max = (sliderMax.value - min) / (max - min);
+        offset_min = offsetwdith - percentage_min * (offsetwdith-knobWidth);
+        lineMin.style.right = `${offset_min}px`;
+        offset_max =  percentage_max * (offsetwdith-knobWidth);
+        lineMax.style.left = `${offset_max}px`;
+    }
+
+    updateLinePosition();
+
+    sliderMin.addEventListener('input', () => {
+        let minValue = parseInt(sliderMin.value);
+        let maxValue = parseInt(sliderMax.value);
+        if (minValue >= maxValue) {
+            sliderMin.value = maxValue - 1; // Ensure a minimum gap
+            inputMin.value = sliderMin.value;
+        }
+        inputMin.value = sliderMin.value;
+        updateLinePosition()
+    });
+
+    sliderMax.addEventListener('input', () => {
+        let minValue = parseInt(sliderMin.value);
+        let maxValue = parseInt(sliderMax.value);
+        if (maxValue <= minValue) {
+            sliderMax.value = minValue + 1; // Ensure a minimum gap
+            inputMax.value = sliderMax.value;
+        }
+        inputMax.value = sliderMax.value;
+        updateLinePosition()
+    });
+
+    inputMin.addEventListener('change', () => {
+        if (parseInt(inputMin.value) >= parseInt(inputMax.value)) {
+            inputMin.value = parseInt(inputMax.value) - 1;
+        }
+        sliderMin.value = inputMin.value;
+        updateLinePosition()
+    });
+
+    inputMax.addEventListener('change', () => {
+        if (parseInt(inputMax.value) <= parseInt(inputMin.value)) {
+            inputMax.value = parseInt(inputMin.value) + 1;
+        }
+        sliderMax.value = inputMax.value;
+        updateLinePosition()
+    });
+
+    sliderMin.addEventListener('input', () => {
+        tableRequest('/filter_range', {column: column, min: sliderMin.value, max: sliderMax.value});
+    });
+    sliderMax.addEventListener('input', () => {
+        tableRequest('/filter_range', {column: column, min: sliderMin.value, max: sliderMax.value});
+    });
+    inputMin.addEventListener('blur', () => {
+        tableRequest('/filter_range', {column: column, min: sliderMin.value, max: sliderMax.value});
+    });
+    inputMax.addEventListener('blur', () => {
+        tableRequest('/filter_range', {column: column, min: sliderMin.value, max: sliderMax.value});
     });
 });
